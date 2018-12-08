6041b6931a31a6dac7b350d8acb5b89c5091d961
David Tran <david.tran@tvtmarine.com>
2018-12-08 18:08:37 +0700

    [MISC] to_backend_theme: fix version string

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "ERPOnline Backend Theme",
     "summary": "Mobile backend theme for Odoo community",
-    "version": "10.0.1.0.23",
+    "version": "11.0.1.0.23",
     "category": "Themes/Backend",
     "website": "https://www.erponline.vn",
 	"description": """
