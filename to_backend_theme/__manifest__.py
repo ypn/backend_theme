9edfc8579a5b2ae8fae4e149242a309c8709be2a
David Tran <david.tran@tvtmarine.com>
2019-03-14 16:27:36 +0700

    [MISC] to_backend_theme: update manifest

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "10.0.1.0.23",
     "category": "Theme/Backend",
     'author' : 'Openworx, T.V.T Marine Automation (aka TVTMA)',
-    'website': 'https://www.tvtmarine.com',
+    'website': 'https://www.erponline.vn',
     'live_test_url': 'https://v10demo-int.erponline.vn',
     'support': 'support@ma.tvtmarine.com',
 	"description": """
