23f52f0c629d0531da865fe4aa7ce1c0cc775be0
David Tran <david.tran@tvtmarine.com>
2019-10-20 15:47:33 +0700

    [UPG] to_backend_theme: Odoo 13 compatible

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016, 2017 Openworx
+# Copyright 2016, 2019 Openworx - Mario Gielissen
+# Copyright 2012, 2019 Openworx - T.V.T Marine Automation
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "ERPOnline Backend Theme",
     "summary": "Mobile backend theme for Odoo community",
-    "version": "12.0.1.0.23",
+    "version": "13.0.1.0.23",
     "category": "Website/Theme/Backend",
     'author' : 'Openworx, T.V.T Marine Automation (aka TVTMA)',
     'website': 'https://www.erponline.vn',
-    'live_test_url': 'https://v12demo-int.erponline.vn',
+    'live_test_url': 'https://v13demo-int.erponline.vn',
     'support': 'support@ma.tvtmarine.com',
 	"description": """
-		Backend theme for Odoo community edition, based on the Material/United Backend Theme by Openworx
+		Backend theme for Odoo community edition, based on the Openworx Backend Theme
     """,
 	'images':[
         'images/erponline_theme_screen.png'
@@ -25,9 +26,11 @@
     ],
     "data": [
         'views/assets.xml',
-        'views/res_company_view.xml',
-        'views/users.xml',
-        'views/sidebar.xml',
+		'views/res_company_view.xml',
+		#'views/users.xml',
+        #'views/sidebar.xml',
     ],
+    #'live_test_url': 'https://youtu.be/JX-ntw2ORl8'
+
 }
 
