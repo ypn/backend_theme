eba37974b8af0ca7c4d7c7b22cd96f06fe63bb9d
David Tran <david.tran@tvtmarine.com>
2021-09-16 11:13:48 +0700

    [MISC] to_backend_theme: standardize manifest

--- to_backend_theme/__manifest__.py
+++ to_backend_theme/__manifest__.py
@@ -1,24 +1,28 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016, 2019 Openworx - Mario Gielissen
 # Copyright 2012, 2019 Openworx - T.V.T Marine Automation
+# Copyright 2019,2021 Viindoo
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Viindoo Backend Theme",
     "summary": "Mobile backend theme for Odoo community",
-    "version": "13.0.1.0.23",
-    "category": "Website/Theme/Backend",
-    'author': 'Openworx, Viindoo',
-    'website': 'https://www.erponline.vn',
+    "version": "1.0.23",
+
+	"description": """
+Backend theme for Viindoo, based on the Openworx Backend Theme
+
+    """,
+
+    'author': 'Openworx,T.V.T Marine Automation,Viindoo',
+    'website': 'https://viindoo.com',
     'live_test_url': 'https://v13demo-int.erponline.vn',
     'support': 'apps.support@viindoo.com',
-	"description": """Backend theme for Viindoo, based on the Openworx Backend Theme""",
-	'images':[
-        'images/screen.png'
-	],
-    "license": "LGPL-3",
-    "installable": True,
-    "auto_install": ['web'],
+
+    # Categories can be used to filter modules in modules listing
+    'category': 'Website/Theme/Backend',
+    'version': '0.1.0',
+
     "depends": [
         'web',
         'web_editor',
@@ -31,5 +35,15 @@
 		# 'views/users.xml',
         # 'views/sidebar.xml',
     ],
+    'images':[
+        'images/screen.png'
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': ['web'],
+    'price': 99.9,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
 }
 
